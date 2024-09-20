c3c8473c30a4701afe4b55f99960359df7f5a32c
srithikab <140123550+srithikab@users.noreply.github.com>
2024-09-20 11:16:52 -0700

    Update PigGame.java

--- PigGame.java
+++ PigGame.java
@@ -6,17 +6,25 @@
  *	@since	
  */
 public class PigGame {
+	private int userTurn = 0;
+	private int userTotal = 0;
+	private int computerTurn = 0;
+	private int computerTotal = 0;
+	private boolean isUserTurn = true;
+	private Dice dice = new Dice();
+	
 	public static void main(String[] args) {
 		PigGame pigG = new PigGame();
 			pigG.printIntroduction();
 			pigG.userChoice();
-			pigG.playGame();
 		}
+		
 	public void userChoice() {
 		boolean check = false;
 		String str = "";
 		do {
 			str = Prompt.getString("Play game or Statistics (p or s) -> ");
+			System.out.println("");
 			if (str.equals("p") || str.equals("s"))
 			{
 				check = true;
@@ -26,33 +34,92 @@ public class PigGame {
 			
 		}
 		else {
-			playGame();
+			play();
 		}
 	}
-	public void playGame() {
-		int userScore = 0;
-		int computerScore = 0;
-		boolean check = false;
+	
+	public void play() {
 		do {
-			System.out.println("Your turn score: " +  userScore);
-			System.out.println("Your total score: " + computerScore);
-			String str = Prompt.getString("(r)oll or (h)old -> ");
-			if (str.equals("r") || str.equals("h"))
-			{
-				if (str.equals("r")) {
-					userTurn();
+			if (isUserTurn) {
+				System.out.println("Your turn score: " +  userTurn);
+				System.out.println("Your total score: " + userTotal);
+				String str = Prompt.getString("(r)oll or (h)old -> ");
+				System.out.println("");
+				if (str.equals("r") || str.equals("h")) {
+					if (str.equals("r")) {
+						userTurn();
+					}
+					else {
+						isUserTurn = false;
+						userTotal += userTurn;
+						userTurn = 0;
+						System.out.println("\nYou HOLD.");
+						System.out.println("Your total score: " + userTotal);
+						System.out.println();
+					}
 				}
-				else {
+			} else if (!isUserTurn) {
+				System.out.println("Computer turn score: " +  computerTurn);
+				System.out.println("Computer total score: " + computerTotal);
+				String str = Prompt.getString("Press enter for computer turn -> ");
+				if (computerTotal + computerTurn < 100) {
+					computerTurn();
+				} else {
+					System.out.println("\nComputer will HOLD.");
+					System.out.println("Computer total score: " + (computerTotal + computerTurn));
+					System.out.println();
+					System.out.println("Too bad. COMPUTER WON.\n");
+					break;
 				}
-				check = true;
 			}
-		} while (check == false || (userScore != 100 ||computerScore != 100));
+		} while (userTotal < 100 && computerTotal < 100);
+		
+		if (userTotal >= 100) {
+			System.out.println("\nUser HOLD.");
+			System.out.println("User total score: " + (userTotal + userTurn));
+			System.out.println();
+			System.out.println("Too bad. USER WON.\n");
+		}
+			
 	}
 	
 	public void userTurn() {
-		
+		int diceVal = dice.roll();
+		System.out.print("\nYou ROLL\n");
+		dice.printDice();
+		System.out.println();
+		if (diceVal == 1) {
+			userTurn = 0;
+			isUserTurn = false;
+			System.out.println("\nYou LOSE your turn.\nYour total score: " + userTotal);
+			System.out.println();
+		}
+		else {
+			userTurn += diceVal;
+		}
 	}
 	
+	public void computerTurn() {
+		int diceVal = dice.roll();
+		System.out.print("\nComputer Will ROLL\n");
+		dice.printDice();
+		System.out.println();
+		if (diceVal == 1) {
+			computerTurn = 0;
+			isUserTurn = true;
+			System.out.print("\nComputer loses turn.\nComputer total score: " + computerTotal);
+			System.out.println("\n");
+		}
+		else {
+			computerTurn += diceVal;
+			if (computerTurn >= 20) {
+				isUserTurn = true;
+				computerTotal += computerTurn;
+				computerTurn = 0;
+			}
+		}
+	}
+		
 	/**	Print the introduction to the game */
 	public void printIntroduction() {
 		System.out.println("\n");
@@ -77,3 +144,4 @@ public class PigGame {
 	}
 	
 }
+
